e408b065c3c29223319828539759b32381d3a416
Jason Garber <jason@sixtwothree.org>
2020-01-13 22:18:24 -0500

    Update Rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ const preamble = `/*!
 `;
 
 export default [
-  // templatetemplate.es.js and templatetemplate.js
+  // templatetemplate.mjs and templatetemplate.js
   {
     input: srcFilePath,
     output: [
@@ -61,7 +61,7 @@ export default [
   {
     input: srcFilePath,
     output: {
-      file: pkg.main.replace(/\.js$/, '.min.js'),
+      file: pkg.browser,
       format: 'umd',
       name: packageName
     },
