cdfa5981f9b3be4d957a36f3e76b478d7d16f3c3
Jason Garber <jason@sixtwothree.org>
2020-01-13 22:16:48 -0500

    Refactor package to ES6/ES2015 module style

--- src/templatetemplate.js
+++ src/templatetemplate.js
@@ -1,34 +1,34 @@
-export default function TemplateTemplate(template, insertions) {
+export default function TemplateTemplate(template, insertions = {}) {
   template = template instanceof HTMLElement ? template : document.querySelector(template);
-  insertions = insertions || {};
 
   if (template === null || !(template instanceof HTMLTemplateElement)) {
-    throw new TypeError(template + ' is not an HTMLTemplateElement');
+    throw new TypeError(`${template} is not an HTMLTemplateElement`);
   }
 
   if (typeof insertions !== 'object') {
-    throw new TypeError(insertions + ' is not an Object');
+    throw new TypeError(`${insertions} is not an Object`);
   }
 
-  var importedNode = document.importNode(template.content, true);
+  const importedNode = document.importNode(template.content, true);
 
-  Object.entries(insertions).forEach(function(insertionArray) {
-    var currentNode = importedNode.querySelector(insertionArray[0]),
-        insertionValue = insertionArray[1];
+  Object.entries(insertions).forEach(([selector, insertion]) => {
+    const currentNode = importedNode.querySelector(selector);
 
-    if (insertionValue instanceof Array) {
-      Object.entries(insertionValue[1]).forEach(function(attributesArray) {
-        currentNode.setAttribute(attributesArray[0], attributesArray[1]);
+    if (insertion instanceof Array) {
+      const [textContent, attributes] = insertion;
+
+      Object.entries(attributes).forEach(([name, value]) => {
+        return currentNode.setAttribute(name, value);
       });
 
-      insertionValue = insertionValue[0];
+      insertion = textContent;
     }
 
-    if (insertionValue instanceof DocumentFragment || insertionValue instanceof HTMLElement) {
-      currentNode.appendChild(insertionValue);
-    } else {
-      currentNode.textContent = insertionValue;
+    if (insertion instanceof DocumentFragment || insertion instanceof HTMLElement) {
+      return currentNode.appendChild(insertion);
     }
+
+    return currentNode.textContent = insertion;
   });
 
   return importedNode;
