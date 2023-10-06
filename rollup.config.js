cc2cbe0c3afbf1758ae03549c4e4a4d5918ed11c
Jason Garber <jason@sixtwothree.org>
2023-10-05 21:50:24 -0400

    Prefer lowercase naming 🤷🏻‍♂️

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import terser from '@rollup/plugin-terser';
 
 const pkg = JSON.parse(readFileSync('./package.json'));
 
-const input = './src/TemplateTemplate.js';
+const input = './src/templatetemplate.js';
 const name = 'TemplateTemplate';
 
 const banner = `/**
