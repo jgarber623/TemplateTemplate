35818376df06c7f2dd91a93e7f73be056c3b5d39
Jason Garber <jason@sixtwothree.org>
2024-02-24 23:22:07 -0500

    Migrate test suite to Node.js assert/test libraries

--- test/templatetemplate.test.js
+++ test/templatetemplate.test.js
@@ -1,5 +1,7 @@
+import assert from 'node:assert';
+import test from 'node:test';
+
 import { JSDOM } from 'jsdom';
-import test from 'ava';
 
 import TemplateTemplate from '../src/templatetemplate.js';
 
@@ -23,26 +25,26 @@ test.before(() => {
 });
 
 test('is a function', t => {
-  t.is(typeof TemplateTemplate, 'function');
+  assert.strictEqual(typeof TemplateTemplate, 'function');
 });
 
 test('when `template` argument is null', t => {
-  t.throws(() => TemplateTemplate(), {
-    instanceOf: TypeError,
-    message: /is not an HTMLTemplateElement$/
+  assert.throws(() => TemplateTemplate(), {
+    name: 'TypeError',
+    message: /is not an HTMLTemplateElement$/,
   });
 });
 
 test('when `template` argument is not an HTMLTemplateElement', t => {
-  t.throws(() => TemplateTemplate(document.createElement('div')), {
-    instanceOf: TypeError,
+  assert.throws(() => TemplateTemplate(document.createElement('div')), {
+    name: 'TypeError',
     message: /is not an HTMLTemplateElement$/
   });
 });
 
 test('when `template` argument references an element that does not exist', t => {
-  t.throws(() => TemplateTemplate('#foo'), {
-    instanceOf: TypeError,
+  assert.throws(() => TemplateTemplate('#foo'), {
+    name: 'TypeError',
     message: /is not an HTMLTemplateElement$/
   });
 });
@@ -50,60 +52,60 @@ test('when `template` argument references an element that does not exist', t => 
 test('when `template` argument is an HTMLElement', t => {
   const renderedTemplate = TemplateTemplate(document.querySelector('#templ'));
 
-  t.true(renderedTemplate instanceof DocumentFragment);
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a></a>');
+  assert.ok(renderedTemplate instanceof DocumentFragment);
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a></a>');
 });
 
 test('when `template` argument is a string', t => {
   const renderedTemplate = TemplateTemplate('#templ');
 
-  t.true(renderedTemplate instanceof DocumentFragment);
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a></a>');
+  assert.ok(renderedTemplate instanceof DocumentFragment);
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a></a>');
 });
 
 test('when `insertions` argument is not an object', t => {
   const template = document.createElement('template');
 
   const expectation = {
-    instanceOf: TypeError,
+    name: 'TypeError',
     message: /is not an Object$/
   };
 
-  t.throws(() => TemplateTemplate(template, 'foo'), expectation);
-  t.throws(() => TemplateTemplate(template, null), expectation);
-  t.throws(() => TemplateTemplate(template, []), expectation);
+  assert.throws(() => TemplateTemplate(template, 'foo'), expectation);
+  assert.throws(() => TemplateTemplate(template, null), expectation);
+  assert.throws(() => TemplateTemplate(template, []), expectation);
 });
 
 test('when `insertions` argument includes a null value', t => {
   const renderedTemplate = TemplateTemplate('#templ', { div: null });
 
-  t.is(renderedTemplate.querySelector('div').outerHTML, '<div><a></a></div>');
+  assert.strictEqual(renderedTemplate.querySelector('div').outerHTML, '<div><a></a></div>');
 });
 
 test('when `insertions` argument includes a string value', t => {
   const renderedTemplate = TemplateTemplate('#templ', { a: 'foo' });
 
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a>foo</a>');
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a>foo</a>');
 });
 
 test('when `insertions` argument includes an array value', t => {
   const insertions = { a: ['foo', { href: 'https://example.com' }] };
   const renderedTemplate = TemplateTemplate('#templ', insertions);
 
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a href="https://example.com">foo</a>');
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a href="https://example.com">foo</a>');
 });
 
 test('when `insertions` argument includes an array value with null text', t => {
   const insertions = { div: [null, { class: 'example' }] };
   const renderedTemplate = TemplateTemplate('#templ', insertions);
 
-  t.is(renderedTemplate.querySelector('div').outerHTML, '<div class="example"><a></a></div>');
+  assert.strictEqual(renderedTemplate.querySelector('div').outerHTML, '<div class="example"><a></a></div>');
 });
 
 test('when `insertions` argument includes an HTMLElement value', t => {
   const renderedTemplate = TemplateTemplate('#templ', { a: document.createElement('span') });
 
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a><span></span></a>');
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a><span></span></a>');
 });
 
 test('when `insertions` argument includes a TemplateTemplate value', t => {
@@ -117,5 +119,5 @@ test('when `insertions` argument includes a TemplateTemplate value', t => {
   const insertions = { a: TemplateTemplate('#templ2', { b: ['foo', { class: 'bar' }] }) };
   const renderedTemplate = TemplateTemplate('#templ', insertions);
 
-  t.is(renderedTemplate.querySelector('div').innerHTML, '<a><b class="bar">foo</b></a>');
+  assert.strictEqual(renderedTemplate.querySelector('div').innerHTML, '<a><b class="bar">foo</b></a>');
 });
