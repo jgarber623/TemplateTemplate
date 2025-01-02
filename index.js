f67bf03c9b17058cfdd4fac9aed545839c7dc93a
Jason Garber <jason@sixtwothree.org>
2025-01-02 14:47:09 -0500

    Remove banner

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/*!
- * @name TemplateTemplate
- * @version 3.0.0
- *
- * @file A very small JavaScript <template> manipulation library.
- *
- * {@link https://github.com/jgarber623/TemplateTemplate}
- *
- * @copyright 2018 Jason Garber (https://sixtwothree.org)
- * @license MIT
- */
-
 /**
  * @param {(HTMLTemplateElement|string)} template An HTML `<template>` element
  *     or a string representing a valid CSS selector referencing an HTML
